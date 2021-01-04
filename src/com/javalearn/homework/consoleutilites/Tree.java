32220f1632d467117121448206a3a02f327dd929
Klaus <neel-cola@mail.ru>
2021-01-04 13:56:54 +0700

    В утилите tree добавлены опции командной строки -a -d -h

--- src/com/javalearn/homework/consoleutilites/Tree.java
+++ src/com/javalearn/homework/consoleutilites/Tree.java
@@ -1,14 +1,21 @@
 package com.javalearn.homework.consoleutilites;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Tree {
 
     static ArrayList<String> levels;
+    static boolean showSize = false;
+    static boolean showHidden = false;
+    static boolean showOnlyDirs = false;
+
 
     public static void main(String[] args) {
 
@@ -21,6 +28,18 @@ public class Tree {
 
         for (String arg : args) {
 
+        }
+        List<String> list = Arrays.asList(args);
+        if (list.indexOf("-h") != -1) {
+            showSize = true;
+        }
+
+        if (list.indexOf("-a") != -1) {
+            showHidden = true;
+        }
+
+        if (list.indexOf("-d") != -1) {
+            showOnlyDirs = true;
         }
 
         levels = new ArrayList<>();
@@ -36,7 +55,12 @@ public class Tree {
             return;
         }
 
-        File subFiles[] = dir.listFiles();
+        File subFiles[] = dir.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                    return (!file.isHidden()||showHidden)&&(file.isDirectory()||!showOnlyDirs);
+            }
+        });
         int idx = 0;
 
 
@@ -54,19 +78,24 @@ public class Tree {
                     levels.set(level, "└--");
                 }
 
-                levels.forEach(System.out::print);
-                idx++;
 
+                idx++;levels.forEach(System.out::print);
+                if (showSize) System.out.print("[" + f.length() + "]");
+
+                System.out.println(f.getName());
                 if (f.isDirectory()) {
+//                    levels.forEach(System.out::print);
                     if (idx - 1 == subFiles.length - 1) {
                         levels.set(level, "   ");
                         levels.add("");
                     }
-                    System.out.println(f.getName());
-
+//                    System.out.println(f.getName());
                     printDirContent(f, level + 1);
                 } else {
-                    System.out.println(f.getName());
+                    if (!showOnlyDirs) {
+
+//                        System.out.println(f.getName());
+                    }
                 }
             }
         levels.remove(levels.size() - 1);
